99cacab7909c61c27cb19577a355c735851829bf
c2hegnoe <c2hegnoe@vasvari.org>
2025-02-17 09:32:36 +0100

    minesmax

--- feladat.py
+++ feladat.py
@@ -4,11 +4,14 @@ for szam in f:
     szam = szam.strip()
     tmp = szam.split(";")
 
+
 def napok(tmp):
     napokSzama=0
+
     for nap in tmp:
         napokSzama += 1
     print("Ennyi nap volt a hónapba: ", napokSzama)
+
 
 def nullakoltes(tmp):
     nullaKoltes=0
@@ -17,13 +20,36 @@ def nullakoltes(tmp):
             nullaKoltes += 1
     print("Ennyi napon nem történt költés: ", nullaKoltes)
 
+
 def atlagkoltes(tmp):
     szum = 0
     for nap in tmp:
         nap = int(nap)
         szum += nap
-    print("Napi átlagköltés: ", szum(len(tmp)), "Ft")
+    print("Napi átlagköltés: ", szum/len(tmp), "Ft")
+
+
+def minesmax(tmp):
+    maxszam = -100000
+    minszam = 100000
+
+    for nap in tmp:
+        nap = int(nap)
+        if nap > maxszam:
+            maxszam = nap
+
+        if nap < minszam and nap!=0:
+            minszam = nap
+
+    print("Legnagyobb vásárlás:", maxszam, "Ft.")
+    print("Legkisebb vásárlás:", minszam, "Ft.")
+
+
+
 
 f.close()
 napok(tmp)
 nullakoltes(tmp)
+atlagkoltes(tmp)
+minesmax(tmp)
+
