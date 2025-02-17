bd408c39b52e7fa894079e2baeb219e2ac218da0
c2hegnoe <c2hegnoe@vasvari.org>
2025-02-17 09:33:22 +0100

    osszes koltes

--- feladat.py
+++ feladat.py
@@ -44,7 +44,13 @@ def minesmax(tmp):
     print("Legnagyobb vásárlás:", maxszam, "Ft.")
     print("Legkisebb vásárlás:", minszam, "Ft.")
 
+def Szum(tmp):
+    szum = 0
+    for nap in tmp:
+        nap = int(nap)
+        szum += nap
 
+    print("Összes költés:", szum, "Ft.")
 
 
 f.close()
@@ -52,4 +58,5 @@ napok(tmp)
 nullakoltes(tmp)
 atlagkoltes(tmp)
 minesmax(tmp)
+Szum(tmp)
 
